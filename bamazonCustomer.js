4fa457469271eebfbcd2a0d8176a0321d7ce41e5
ccruml20 <ccrumley1@gmail.com>
2017-08-02 01:56:20 -0600

    decremented mysqldata set

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,12 +1,36 @@
+var inquirer = require('inquirer');
 var DataAccessLayer = require("./dataAccessLayer.js");
 var dataAccess = new DataAccessLayer(3306, "root", "password");
+
 dataAccess.getProducts(function(productArray){
-  console.log("-------------",productArray[0]);
-});
 
+	function customerPromt(){
+		inquirer.prompt([
+		{
+			type: "input",
+			message: "Enter the Item Id for the product you wish to purchase.",
+			name: "item_id",
+		},
+		{
+			type: "input",
+			message: "How many would you like to purchase?",
+			name: "stock_quantity",
+		},
+		])
+		.then(purchasedItem);
+	};
 
+	var purchasedItem = function(customerRes){
+			for (var i = 0; i < productArray.length; i++) {
+				if(customerRes.item_id == productArray[i].item){
+					var order = productArray[i];
+				}
+			}			
+			dataAccess.setProducts(customerRes);
+		}
 
-// function orderProduct(productOrdered){
-//   console.log("================",productOrdered);
-// }
+		customerPromt();
+		
+	});
+
 
--- dataAccessLayer.js
+++ dataAccessLayer.js
@@ -4,10 +4,10 @@ var DataAccessLayer = function(port, user, password) {
 	var connection = mysql.createConnection({
 		host: "localhost",
 		port: port,
-        user: user,
-        password: password,
-        database: "bamazon"
-    });
+		user: user,
+		password: password,
+		database: "bamazon"
+	});
 	this.getProducts = function(callback){
 		connection.connect(function(err) {
 			if (err) throw err;
@@ -19,13 +19,24 @@ var DataAccessLayer = function(port, user, password) {
 					console.log("Item Id: " + res[i].item_id + " || Product: " + res[i].product_name + " || Price: " + res[i].price);
 					productArray.push(new Product(res[i]));
 				}
-				connection.end(function(fail) {
-					if (fail) throw fail;
-				});
+				// connection.end(function(fail) {
+				// 	if (fail) throw fail;
+				// });
 				callback(productArray);
 			});
 		});
 	};
+	this.setProducts = function(customerRes, callback){
+		console.log("this is the inquirer answers",customerRes);
+		connection.connect(function(err) {
+			console.log("Updating Bamazon...\n",);
+			var query = "UPDATE products SET stock_quantity=? WHERE item_id=?";
+			connection.query(query, [customerRes.stock_quantity, customerRes.item_id], function(err, res) {
+				if (err) throw err;	
+				console.log(res);	
+			});
+		});
+	}
 }
 
 module.exports = DataAccessLayer;
